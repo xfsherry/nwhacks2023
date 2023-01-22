a6f2986bcef71418bc4db7c3ab56e75221cf310c
Steven Wong <stevenwong@Stevens-MacBook-Pro.local>
2023-01-21 17:48:15 -0800

    add image upload client

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import ImageUpload from './Components/ImageUpload';
 import Header from './Components/Header';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <Header className="App-header">
           Plants
       </Header>
+      <ImageUpload/>
     </div>
   );
 }
---
+++ client/src/Components/ImageUpload.js
@@ -0,0 +1,61 @@
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
+import {styled} from "@mui/material/styles";
+import React, {useState} from "react";
+
+
+const Input = styled("input")({
+    display: "none",
+});
+
+const toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+});
+
+
+const ImageUpload = () => {
+const [image, setImage] = useState(undefined);
+return (
+    <div>
+        <label htmlFor="icon-button-file">
+        <Input
+                    onChange={async (e) => {
+                        const imageFile = e.target.files[0];
+                        setImage(imageFile);
+                        const base64Image = await toBase64(imageFile);
+                        console.log(base64Image);
+                        
+                        e.target.value = null;
+                    }}
+                    accept="image/*"
+                    id="icon-button-file"
+                    type="file"
+                />
+           
+            <IconButton
+                color="primary"
+                aria-label="upload picture"
+                component="span"
+            >
+                <PhotoCamera/>
+            </IconButton>
+            <p>{image ? image.name : ""}</p>
+        </label>
+        <div className="p-2">
+            <Button  variant="outlined">
+                Upload
+            </Button>
+        </div>
+    </div>
+);
+
+
+}
+
+
+
+export default ImageUpload;
