79db5663970bcbf237d5e753bbbaccea4460830f
Cody Sayo <codysayo@gmail.com>
2023-01-22 01:04:12 -0800

    test camera

---
+++ mobile_client/camera/CameraView.js
@@ -0,0 +1,167 @@
+// import { Camera, CameraType } from 'expo-camera';
+// import { useState } from 'react';
+// import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+// export default function CameraView() {
+//   const [type, setType] = useState(CameraType.back);
+//   const [permission, requestPermission] = Camera.useCameraPermissions();
+
+//   if (!permission) {
+//     // Camera permissions are still loading
+//     return <View />;
+//   }
+
+//   if (!permission.granted) {
+//     // Camera permissions are not granted yet
+//     return (
+//       <View style={styles.container}>
+//         <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+//         <Button onPress={requestPermission} title="grant permission" />
+//       </View>
+//     );
+//   }
+
+//   function toggleCameraType() {
+//     setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+//   }
+
+//   return (
+//     <View style={styles.container}>
+//       {/* <Camera style={styles.camera} type={type}>
+//         <View style={styles.buttonContainer}>
+//           <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+//             <Text style={styles.text}>Flip Camera</Text>
+//           </TouchableOpacity>
+//         </View>
+//       </Camera> */}
+//       <WebView
+//     originWhitelist={['*']}
+//     source={{ html: `<label for='cameraFileInput'>
+//       <Text class='btn'>Open camera</Text>
+
+//       <input
+//         id='cameraFileInput'
+//         type='file'
+//         accept='image/*'
+//         capture='environment'
+//       />
+//     </label>` }}
+
+
+
+// />
+
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//   },
+//   camera: {
+//     flex: 0,
+//   },
+//   buttonContainer: {
+//     flex: 1,
+//     flexDirection: 'row',
+//     backgroundColor: 'transparent',
+//     margin: 64,
+//   },
+//   button: {
+//     flex: 1,
+//     alignSelf: 'flex-end',
+//     alignItems: 'center',
+//   },
+//   text: {
+//     fontSize: 24,
+//     fontWeight: 'bold',
+//     color: 'white',
+//   },
+// });
+
+import React from 'react';
+import { Camera } from 'expo-camera';
+import { View, Text } from 'react-native';
+import * as Permissions from 'expo-permissions';
+
+import styles from './styles';
+import Toolbar from './toolbar.component';
+import Gallery from './gallery.component';
+
+export default class CameraView extends React.Component {
+    camera = null;
+
+    state = {
+        captures: [],
+        capturing: null,
+        hasCameraPermission: null,
+        cameraType: Camera.Constants.Type.back,
+        flashMode: Camera.Constants.FlashMode.off,
+    };
+
+    setFlashMode = (flashMode) => this.setState({ flashMode });
+    setCameraType = (cameraType) => this.setState({ cameraType });
+    handleCaptureIn = () => this.setState({ capturing: true });
+
+    handleCaptureOut = () => {
+        if (this.state.capturing)
+            this.camera.stopRecording();
+    };
+
+    handleShortCapture = async () => {
+        const photoData = await this.camera.takePictureAsync();
+        this.setState({ capturing: false, captures: [photoData, ...this.state.captures] })
+    };
+
+    handleLongCapture = async () => {
+        const videoData = await this.camera.recordAsync();
+        this.setState({ capturing: false, captures: [videoData, ...this.state.captures] });
+    };
+
+    async componentDidMount() {
+        const camera = await Permissions.askAsync(Permissions.CAMERA);
+        const audio = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+        const hasCameraPermission = (camera.status === 'granted' && audio.status === 'granted');
+
+        this.setState({ hasCameraPermission });
+    };
+
+    render() {
+        const { hasCameraPermission, flashMode, cameraType, capturing, captures } = this.state;
+
+        if (hasCameraPermission === null) {
+            return <View />;
+        } else if (hasCameraPermission === false) {
+            return <Text>Access to camera has been denied.</Text>;
+        }
+
+        return (
+            <React.Fragment>
+                <View>
+                    <Camera
+                        type={cameraType}
+                        flashMode={flashMode}
+                        style={styles.preview}
+                        ref={camera => this.camera = camera}
+                    />
+                </View>
+
+                {captures.length > 0 && <Gallery captures={captures}/>}
+
+                <Toolbar 
+                    capturing={capturing}
+                    flashMode={flashMode}
+                    cameraType={cameraType}
+                    setFlashMode={this.setFlashMode}
+                    setCameraType={this.setCameraType}
+                    onCaptureIn={this.handleCaptureIn}
+                    onCaptureOut={this.handleCaptureOut}
+                    onLongCapture={this.handleLongCapture}
+                    onShortCapture={this.handleShortCapture}
+                />
+            </React.Fragment>
+        );
+    };
+};
